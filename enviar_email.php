05aa4630d948c162a8c6f04c1e055d5da5aa4e5c
Thiago <163183071+thiluis79@users.noreply.github.com>
2024-12-20 11:03:49 -0300

    Update enviar_email.php

--- enviar_email.php
+++ enviar_email.php
@@ -3,10 +3,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
 
-    $to = "seuemail@dominio.com"; // Substitua pelo e-mail de destino
+    $to = "thi.luis@gmail.com"; // Substitua pelo e-mail de destino
     $subject = "Novo formulário enviado";
     $message = "Nome: " . $nome . "\nE-mail: " . $email;
-    $headers = "From: webmaster@seudominio.com"; // Ajuste para o e-mail do seu servidor
+    $headers = "From: thiago.casacio@webliv.com"; // Ajuste para o e-mail do seu servidor
 
     if (mail($to, $subject, $message, $headers)) {
         echo "E-mail enviado com sucesso!";
